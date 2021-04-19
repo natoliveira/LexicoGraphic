fe3832dafc75d17708d3ea6093c0a2d3ef3ea78b
natoliveira <nataliadeolive@gmail.com>
2021-04-18 22:24:57 -0300

    implementando algoritmo LexicoGraphic

---
+++ cmake-build-debug/CMakeFiles/untitled19.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled19.dir/Lexicographic.cpp.o"
+  "untitled19"
+  "untitled19.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
